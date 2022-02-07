ed5f32622fc40d23193ef1f62058ae8bd1904197
Felipe Gómez <luis.andrade18@hotmail.com>
2022-02-06 21:50:47 -0500

    first commit

---
+++ app/src/main/java/edu/co/icesi/cisco_login_interface_practice/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.co.icesi.cisco_login_interface_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
